c927bfdd9d5047f8c0379557ab5ce95b403b3324
asamur <arif.samur@caresyntax.com>
2022-07-29 13:53:21 -0400

    New command

--- src/test/java/com/platform/runners/CukesRunner.java
+++ src/test/java/com/platform/runners/CukesRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
     features = "src/test/resources/features",
     glue = "com/platform/step_definitions",
     dryRun = false,
-    tags = "@qa"   //"@Surgeon or @KPI or @Smoke or @Locale"
+    tags = "@smoke"   //"@Surgeon or @KPI or @Smoke or @Locale"
 )
 public class CukesRunner {
 
